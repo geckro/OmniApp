7bd846ee2a4a9cf66e4cdddab87ac17dd87cd5e2
geckro <94733731+geckro@users.noreply.github.com>
2024-07-09 16:52:09 +0800

    Add boolean properties for various metadata in Game class

--- GameManager.Core/Data/DataStructures.cs
+++ GameManager.Core/Data/DataStructures.cs
@@ -7,7 +7,23 @@ public class Game(
     DateTime? date = null,
     Developer[]? developers = null,
     Publisher[]? publishers = null,
-    Series[]? series = null) : IMetadata
+    Series[]? series = null,
+    bool? isRemake = null,
+    Game? remakeOf = null,
+    bool? isRemaster = null,
+    Game? remasterOf = null,
+    bool? isPort = null,
+    Game? portOf = null,
+    bool? isSequel = null,
+    Game? sequelOf = null,
+    bool? isPrequel = null,
+    Game? prequelOf = null,
+    bool? isDlc = null,
+    Game? dlcOf = null,
+    bool? isMod = null,
+    Game? modOf = null,
+    bool? isHack = null,
+    Game? hackOf = null) : IMetadata
 {
     public string Title { get; } = title;
     public Genre[]? Genres { get; } = genres;
@@ -16,6 +32,24 @@ public class Game(
     public Developer[]? Developers { get; } = developers;
     public Publisher[]? Publishers { get; } = publishers;
     public Series[]? Series { get; } = series;
+
+    public bool? IsRemake { get; } = isRemake;
+    public Game? RemakeOf { get; } = remakeOf;
+    public bool? IsRemaster { get; } = isRemaster;
+    public Game? RemasterOf { get; } = remasterOf;
+    public bool? IsPort { get; } = isPort;
+    public Game? PortOf { get; } = portOf;
+    public bool? IsSequel { get; } = isSequel;
+    public Game? SequelOf { get; } = sequelOf;
+    public bool? IsPrequel { get; } = isPrequel;
+    public Game? PrequelOf { get; } = prequelOf;
+
+    public bool? IsDlc { get; } = isDlc;
+    public Game? DlcOf { get; } = dlcOf;
+    public bool? IsMod { get; } = isMod;
+    public Game? ModOf { get; } = modOf;
+    public bool? IsHack { get; } = isHack;
+    public Game? HackOf { get; } = hackOf;
 
     string IMetadata.Name => Title;
 }
