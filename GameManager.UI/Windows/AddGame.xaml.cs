49ec48271eb9e673d51f46f8e5bbb514d0fb02ce
geckro <94733731+geckro@users.noreply.github.com>
2024-07-10 15:24:37 +0800

    Remove menus with ListViews

--- GameManager.UI/Windows/AddGame.xaml.cs
+++ GameManager.UI/Windows/AddGame.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using GameManager.Core.Data;
-using GameManager.UI.Helpers;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -11,65 +10,53 @@ public partial class AddGame
     {
         InitializeComponent();
 
-        Menus menuHelper = new();
-        menuHelper.InitializePlatformsMenu(PlatformsMenu);
-        menuHelper.InitializeGenresMenu(GenresMenu);
-        menuHelper.InitializeDevelopersMenu(DevelopersMenu);
-        menuHelper.InitializePublishersMenu(PublishersMenu);
-        menuHelper.InitializeSeriesMenu(SeriesMenu);
+        InitializeListView(GenresListView, new GenreData().Deserialize());
+        InitializeListView(PlatformsListView, new PlatformData().Deserialize());
+        InitializeListView(DevelopersListView, new DeveloperData().Deserialize());
+        InitializeListView(PublishersListView, new PublisherData().Deserialize());
+        InitializeListView(SeriesListView, new SeriesData().Deserialize());
+    }
+
+    private void InitializeListView<T>(ListView listView, IEnumerable<T> metadataList) where T : IMetadata
+    {
+        foreach (T metadata in metadataList)
+        {
+            CheckBox checkBox = new() { Content = metadata.Name };
+            listView.Items.Add(checkBox);
+        }
     }
 
     private void AddNewGameFinish_Click(object sender, RoutedEventArgs e)
     {
-        string title = TitleBox.Text;
-
-        if (string.IsNullOrWhiteSpace(title))
-        {
-            MessageBox.Show(
-                "Please enter a valid title.",
-                "Error",
-                MessageBoxButton.OK,
-                MessageBoxImage.Error
-            );
-            return;
-        }
-
-        List<Genre> genres = (from MenuItem item in GenresMenu.Items
-            where item.IsChecked
-            select new Genre(item.Header.ToString())).ToList();
-
-        List<Platform> platforms = (from MenuItem item in PlatformsMenu.Items
-            where item.IsChecked
-            select new Platform{Name = item.Header.ToString()}).ToList();
-
-        List<Developer> developers = (from MenuItem item in DevelopersMenu.Items
-            where item.IsChecked
-            select new Developer(item.Header.ToString())).ToList();
-
-        List<Publisher> publishers = (from MenuItem item in PublishersMenu.Items
-            where item.IsChecked
-            select new Publisher(item.Header.ToString())).ToList();
-
-        List<Series> series = (from MenuItem item in SeriesMenu.Items
-            where item.IsChecked
-            select new Series(item.Header.ToString())).ToList();
-
-        DateTime? dateWw = Date.SelectedDate;
-
-        Game newGame = new()
-        {
-            Title = title,
-            Genres = genres,
-            Platforms = platforms,
-            Developers = developers,
-            Publishers = publishers,
-            Series = series,
-            ReleaseDateWw = dateWw,
-            CreatedOn = DateTime.Now,
-            LastUpdated = DateTime.Now
-        };
-
-        GameData gameData = new();
-        gameData.Add(newGame);
+        // string title = TitleBox.Text;
+        //
+        // if (string.IsNullOrWhiteSpace(title))
+        // {
+        //     MessageBox.Show(
+        //         "Please enter a valid title.",
+        //         "Error",
+        //         MessageBoxButton.OK,
+        //         MessageBoxImage.Error
+        //     );
+        //     return;
+        // }
+        //
+        // DateTime? dateWw = Date.SelectedDate;
+        //
+        // Game newGame = new()
+        // {
+        //     Title = title,
+        //     Genres = genres,
+        //     Platforms = platforms,
+        //     Developers = developers,
+        //     Publishers = publishers,
+        //     Series = series,
+        //     ReleaseDateWw = dateWw,
+        //     CreatedOn = DateTime.Now,
+        //     LastUpdated = DateTime.Now
+        // };
+        //
+        // GameData gameData = new();
+        // gameData.Add(newGame);
     }
 }
