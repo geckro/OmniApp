c63ccdc39da5da4e730ed08378591127e458584a
geckro <94733731+geckro@users.noreply.github.com>
2024-07-11 22:28:59 +0800

    blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue blue

--- src/GameManager.UI/Helpers/DataGridHelper.cs
+++ src/GameManager.UI/Helpers/DataGridHelper.cs
@@ -1,5 +1,6 @@
 ﻿using GameManager.Core.Data;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace GameManager.UI.Helpers;
 
@@ -8,6 +9,62 @@ public class DataGridHelper
     public void UpdateGameDataGrid(DataGrid dataGrid)
     {
         ICollection<Game> games = new DataManagerFactory().CreateData<Game>().ReadFromJson();
+
+        dataGrid.AutoGenerateColumns = false;
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Title",
+            Binding = new Binding("Title")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Played",
+            Binding = new Binding("HasPlayed")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Finished",
+            Binding = new Binding("HasFinished")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Complete",
+            Binding = new Binding("HasCompleted")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Genres",
+            Binding = new Binding("Genres")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Developers",
+            Binding = new Binding("Developers")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Publishers",
+            Binding = new Binding("Publishers")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Series",
+            Binding = new Binding("Series")
+        });
+
+        dataGrid.Columns.Add(new DataGridTextColumn
+        {
+            Header = "Date Released",
+            Binding = new Binding("ReleaseDateWw")
+        });
 
         dataGrid.Items.Clear();
         dataGrid.ItemsSource = games;
--- src/GameManager.UI/Windows/MainWindow.xaml.cs
+++ src/GameManager.UI/Windows/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using GameManager.UI.Helpers;
 using OmniApp.Common.Logging;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GameManager.UI.Windows;
 
@@ -16,6 +17,8 @@ public partial class MainWindow
         InitializeComponent();
 
         UpdateGameDataGrid();
+
+        PopulateDataGridContextMenu();
     }
 
     private void AddButton_Click(object sender, RoutedEventArgs e)
@@ -31,5 +34,36 @@ public partial class MainWindow
     public void UpdateGameDataGrid()
     {
         _dataGridHelper.UpdateGameDataGrid(GameDataGrid);
+    }
+
+    private void PopulateDataGridContextMenu()
+    {
+        ContextMenu contextMenu = GameDataGridContextMenu;
+
+        IEnumerable<string> metadata = ["played", "finished", "completed"];
+        foreach (string data in metadata)
+        {
+            MenuItem menuItem = new()
+            {
+                Header = $"Mark as {data}",
+                IsCheckable = true
+            };
+            contextMenu.Items.Add(menuItem);
+        }
+    }
+
+    private void MarkPlayed_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void MarkFinished_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void MarkCompleted_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
