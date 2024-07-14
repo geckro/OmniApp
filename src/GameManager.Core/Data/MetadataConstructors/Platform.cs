afb46ec7178485ab551b3acc30d0a7b5e931d87e
geckro <94733731+geckro@users.noreply.github.com>
2024-07-14 16:29:40 +0800

    Make platform string required

--- src/GameManager.Core/Data/MetadataConstructors/Platform.cs
+++ src/GameManager.Core/Data/MetadataConstructors/Platform.cs
@@ -7,13 +7,13 @@ public class Platform : IMetadata
 {
     public required Guid Id { get; set; }
 
+    public required string Name { get; set; }
+
     public string? Company { get; set; }
 
     public Uri? Logo { get; set; }
 
     public PlatformType? Type { get; set; }
-
-    public string Name { get; set; }
 
     [JsonIgnore] public string JsonFile => "platforms.json";
 }
