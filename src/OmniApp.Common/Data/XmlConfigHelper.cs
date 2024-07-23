a5fa5f6bce11e934a85cdc413c046a0f2bcf363a
geckro <94733731+geckro@users.noreply.github.com>
2024-07-23 16:23:43 +0800

    Add XML config helper. Not tested

---
+++ src/OmniApp.Common/Data/XmlConfigHelper.cs
@@ -0,0 +1,64 @@
+﻿using OmniApp.Common.Logging;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace OmniApp.Common.Data;
+
+/// <summary>
+///     Helper class to help read and write data to and from XML, for config.
+/// </summary>
+public class XmlConfigHelper(string configFilePath)
+{
+    private XDocument LoadConfig()
+    {
+        if (!File.Exists(configFilePath))
+        {
+            return new XDocument(new XElement(XName.Get("Preferences")));
+        }
+
+        try
+        {
+            return XDocument.Load(configFilePath);
+        }
+        catch (XmlException)
+        {
+            return new XDocument(new XElement(XName.Get("Preferences")));
+        }
+    }
+
+    public void WriteToConfig(XName key, string value)
+    {
+        XDocument doc = LoadConfig();
+        XElement? element = doc.Root?.Element(key);
+
+        if (element != null)
+        {
+            element.Value = value;
+        }
+        else
+        {
+            doc.Root?.Add(new XElement(key, value));
+        }
+
+        try
+        {
+            doc.Save(configFilePath);
+        }
+        catch (IOException ex)
+        {
+            Logger.Error(LogClass.OmniCommon, $"Unable to save config file {ex}");
+        }
+    }
+
+    public string? ReadFromConfig(XName key)
+    {
+        XDocument doc = LoadConfig();
+        return doc.Root?.Element(key)?.Value;
+    }
+
+    public bool KeyExists(XName key)
+    {
+        XDocument doc = LoadConfig();
+        return doc.Root?.Element(key) != null;
+    }
+}
