68b670603dc884d6d6c6b7302abe40ba152bd9b3
geckro <94733731+geckro@users.noreply.github.com>
2024-07-31 20:59:55 +0800

    Move RelayCommand to its own file

--- src/GameManager.UI/ViewModels/MainGameWindowViewModel.cs
+++ src/GameManager.UI/ViewModels/MainGameWindowViewModel.cs
@@ -111,38 +111,3 @@ public class MainGameWindowViewModel
         MessageBox.Show("Delete");
     }
 }
-
-public class RelayCommand<T>(Action<T> execute, Func<T, bool>? canExecute = null) : ICommand
-{
-    private readonly Action<T> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-
-    public event EventHandler? CanExecuteChanged
-    {
-        add => CommandManager.RequerySuggested += value;
-        remove => CommandManager.RequerySuggested -= value;
-    }
-
-    public bool CanExecute(object? parameter)
-    {
-        bool result = canExecute == null
-                      || (parameter == null && typeof(T) == typeof(object))
-                      || (parameter is T arg && canExecute(arg));
-        return result;
-    }
-
-    public void Execute(object? parameter)
-    {
-        switch (parameter)
-        {
-            case T arg:
-                _execute(arg);
-                break;
-            case null when typeof(T) == typeof(object):
-                _execute(default!);
-                break;
-            default:
-                Logger.Error(LogClass.GameMgrUi, $"Parameter type mismatch. Expected {typeof(T).Name}, got {parameter?.GetType().Name ?? "null"}");
-                break;
-        }
-    }
-}
