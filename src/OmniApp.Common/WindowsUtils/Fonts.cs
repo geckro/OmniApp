cb5402fa4960cfc2232a4b2e83e6ce5c51ac61ec
geckro <94733731+geckro@users.noreply.github.com>
2024-08-01 18:56:40 +0800

    Add Fonts class and GetFonts method. This will make this app Windows-only, as System.Drawing.Common is Windows-only. LOL!

---
+++ src/OmniApp.Common/WindowsUtils/Fonts.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing.Text;
+
+namespace OmniApp.Common.WindowsUtils;
+
+public class Fonts
+{
+    public static List<string> GetInstalledFonts()
+    {
+        List<string> fonts = [];
+        InstalledFontCollection installedFonts = new();
+        fonts.AddRange(installedFonts.Families.Select(font => font.Name));
+
+        return fonts;
+    }
+}
