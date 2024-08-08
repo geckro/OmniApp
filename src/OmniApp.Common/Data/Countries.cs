f77cfaa61fb05f19a09ab96c0ef5ea9a1b43041c
geckro <94733731+geckro@users.noreply.github.com>
2024-08-08 14:42:29 +0800

    perf: Change country classes to records

--- src/OmniApp.Common/Data/Countries.cs
+++ src/OmniApp.Common/Data/Countries.cs
@@ -5,25 +5,25 @@ public interface ICountry
     string Name { get; }
 }
 
-public class Albania : ICountry
+public record Albania : ICountry
 {
     public string NativeName => "Shqipëri";
     public string Name => "Albania";
 }
 
-public class Algeria : ICountry
+public record Algeria : ICountry
 {
     public string NativeName => "الجزائر";
     public string NativeNameTranslation => "al-Jazāʾir";
     public string Name => "Algeria";
 }
 
-public class Argentina : ICountry
+public record Argentina : ICountry
 {
     public string Name => "Argentina";
 }
 
-public class Austria : ICountry
+public record Austria : ICountry
 {
     public string NativeName => "Österreich";
     public string Demonym => "Austrian";
@@ -34,7 +34,7 @@ public class Austria : ICountry
     public string Name => "Austria";
 }
 
-public class Australia : ICountry
+public record Australia : ICountry
 {
     public string Demonym => "Australian";
     public IEnumerable<string> Languages => ["English"];
@@ -44,20 +44,20 @@ public class Australia : ICountry
     public string Name => "Australia";
 }
 
-public class Bangladesh : ICountry
+public record Bangladesh : ICountry
 {
     public string NativeName => "বাংলাদেশ";
     public string NativeNameTranslation => "Bāṅlādēś";
     public string Name => "Bangladesh";
 }
 
-public class Belarus : ICountry
+public record Belarus : ICountry
 {
     public string NativeName => "Беларусь";
     public string Name => "Belarus";
 }
 
-public class Belgium : ICountry
+public record Belgium : ICountry
 {
     public string Demonym => "Belgian";
     public string NativeName => "Koninkrijk België";
@@ -68,18 +68,18 @@ public class Belgium : ICountry
     public string Name => "Belgium";
 }
 
-public class Bolivia : ICountry
+public record Bolivia : ICountry
 {
     public string Name => "Bolivia";
 }
 
-public class Brazil : ICountry
+public record Brazil : ICountry
 {
     public string NativeName => "Brasil";
     public string Name => "Brazil";
 }
 
-public class Bulgaria : ICountry
+public record Bulgaria : ICountry
 {
     public string Demonym => "Bulgarian";
     public string NativeName => "България";
@@ -87,7 +87,7 @@ public class Bulgaria : ICountry
     public string Name => "Bulgaria";
 }
 
-public class Canada : ICountry
+public record Canada : ICountry
 {
     public string Demonym => "Canadian";
     public IEnumerable<string> Languages => ["English"];
@@ -97,12 +97,12 @@ public class Canada : ICountry
     public string Name => "Canada";
 }
 
-public class Chile : ICountry
+public record Chile : ICountry
 {
     public string Name => "Chile";
 }
 
-public class China : ICountry
+public record China : ICountry
 {
     public string NativeName => "中国";
     public string NativeNameTranslation => "Zhōngguó";
@@ -114,80 +114,80 @@ public class China : ICountry
     public string Name => "China (People's Republic)";
 }
 
-public class Colombia : ICountry
+public record Colombia : ICountry
 {
     public string Name => "Colombia";
 }
 
-public class CostaRica : ICountry
+public record CostaRica : ICountry
 {
     public string Name => "Costa Rica";
 }
 
-public class Croatia : ICountry
+public record Croatia : ICountry
 {
     public string NativeName => "Hrvatska";
     public string Name => "Croatia";
 }
 
-public class Cuba : ICountry
+public record Cuba : ICountry
 {
     public string Name => "Cuba";
 }
 
-public class Czechia : ICountry
+public record Czechia : ICountry
 {
     public string NativeName => "Česko";
     public string Name => "Czech Republic";
 }
 
-public class Denmark : ICountry
+public record Denmark : ICountry
 {
     public string NativeName => "Danmark";
     public string Name => "Denmark";
 }
 
-public class DominicanRepublic : ICountry
+public record DominicanRepublic : ICountry
 {
     public string NativeName => "República Dominicana";
     public string Name => "Dominican Republic";
 }
 
-public class Ecuador : ICountry
+public record Ecuador : ICountry
 {
     public string Name => "Ecuador";
 }
 
-public class Egypt : ICountry
+public record Egypt : ICountry
 {
     public string NativeName => "مصر";
     public string NativeNameTranslation => "Miṣr";
     public string Name => "Egypt";
 }
 
-public class ElSalvador : ICountry
+public record ElSalvador : ICountry
 {
     public string Name => "El Salvador";
 }
 
-public class Estonia : ICountry
+public record Estonia : ICountry
 {
     public string NativeName => "Eesti";
     public string Name => "Estonia";
 }
 
-public class Finland : ICountry
+public record Finland : ICountry
 {
     public string NativeName => "Suomi";
     public string Name => "Finland";
 }
 
-public class France : ICountry
+public record France : ICountry
 {
     public string Name => "France";
 }
 
-public class Germany : ICountry
+public record Germany : ICountry
 {
     public string NativeName => "Deutschland";
     public string Demonym => "German";
@@ -198,74 +198,74 @@ public class Germany : ICountry
     public string Name => "Germany";
 }
 
-public class Greece : ICountry
+public record Greece : ICountry
 {
     public string NativeName => "Ελλάδα";
     public string NativeNameTranslation => "Elláda";
     public string Name => "Greece";
 }
 
-public class Guatemala : ICountry
+public record Guatemala : ICountry
 {
     public string Name => "Guatemala";
 }
 
-public class Haiti : ICountry
+public record Haiti : ICountry
 {
     public string NativeName => "Ayiti";
     public string Name => "Haiti";
 }
 
-public class Honduras : ICountry
+public record Honduras : ICountry
 {
     public string Name => "Honduras";
 }
 
-public class Hungary : ICountry
+public record Hungary : ICountry
 {
     public string NativeName => "Magyarország";
     public string Name => "Hungary";
 }
 
-public class India : ICountry
+public record India : ICountry
 {
     public string NativeName => "भारत";
     public string NativeNameTranslation => "Bhaarat";
     public string Name => "India";
 }
 
-public class Indonesia : ICountry
+public record Indonesia : ICountry
 {
     public string Name => "Indonesia";
 }
 
-public class Iran : ICountry
+public record Iran : ICountry
 {
     public string NativeName => "ایران";
     public string Name => "Iran";
 }
 
-public class Iraq : ICountry
+public record Iraq : ICountry
 {
     public string NativeName => "ٱلْعِرَاق";
     public string NativeNameTranslation => "Êraq";
     public string Name => "Iraq";
 }
 
-public class Ireland : ICountry
+public record Ireland : ICountry
 {
     public string NativeName => "Éire";
     public string Name => "Ireland";
 }
 
-public class Israel : ICountry
+public record Israel : ICountry
 {
     public string NativeName => "יִשְׂרָאֵל";
     public string NativeNameTranslation => "Yīsrāʾēl";
     public string Name => "Israel";
 }
 
-public class Italy : ICountry
+public record Italy : ICountry
 {
     public string NativeName => "Italia";
     public IEnumerable<string> Languages => ["Italian"];
@@ -275,7 +275,7 @@ public class Italy : ICountry
     public string Name => "Italy";
 }
 
-public class Japan : ICountry
+public record Japan : ICountry
 {
     public string NativeName => "日本国";
     public string NativeNameTranslation => "Nihon";
@@ -287,47 +287,47 @@ public class Japan : ICountry
     public string Name => "Japan";
 }
 
-public class Latvia : ICountry
+public record Latvia : ICountry
 {
     public string NativeName => "Latvijas";
     public string Name => "Latvia";
 }
 
-public class Libya : ICountry
+public record Libya : ICountry
 {
     public string NativeName => "ليبيا";
     public string NativeNameTranslation => "Lībiyā";
     public string Name => "Libya";
 }
 
-public class Lithuania : ICountry
+public record Lithuania : ICountry
 {
     public string NativeName => "Lietuva";
     public string Name => "Lithuania";
 }
 
-public class Luxembourg : ICountry
+public record Luxembourg : ICountry
 {
     public string NativeName => "Lëtzebuerg";
     public string Name => "Luxembourg";
 }
 
-public class Malaysia : ICountry
+public record Malaysia : ICountry
 {
     public string Name => "Malaysia";
 }
 
-public class Mexico : ICountry
+public record Mexico : ICountry
 {
     public string Name => "Mexico";
 }
 
-public class Moldova : ICountry
+public record Moldova : ICountry
 {
     public string Name => "Moldova";
 }
 
-public class Netherlands : ICountry
+public record Netherlands : ICountry
 {
     public string NativeName => "Nederland";
     public IEnumerable<string> Languages => ["Dutch"];
@@ -337,72 +337,72 @@ public class Netherlands : ICountry
     public string Name => "Netherlands";
 }
 
-public class NewZealand : ICountry
+public record NewZealand : ICountry
 {
     public string NativeName => "Aotearoa";
     public string Name => "New Zealand";
 }
 
-public class Nicaragua : ICountry
+public record Nicaragua : ICountry
 {
     public string Name => "Nicaragua";
 }
 
-public class Nigeria : ICountry
+public record Nigeria : ICountry
 {
     public string NativeName => "Najeriya";
     public string Name => "Nigeria";
 }
 
-public class NorthKorea : ICountry
+public record NorthKorea : ICountry
 {
     public string NativeName => "조선";
     public string NativeNameTranslation => "Chosŏn";
     public string Name => "North Korea";
 }
 
-public class Norway : ICountry
+public record Norway : ICountry
 {
     public string NativeName => "Norge";
     public string Name => "Norway";
 }
 
-public class Oman : ICountry
+public record Oman : ICountry
 {
     public string NativeName => "عُمَان";
     public string NativeNameTranslation => "ʿUmān";
     public string Name => "Oman";
 }
 
-public class Pakistan : ICountry
+public record Pakistan : ICountry
 {
     public string NativeName => "پَاکِسْتَان";
     public string Name => "Pakistan";
 }
 
-public class Palestine : ICountry
+public record Palestine : ICountry
 {
     public string NativeName => "فلسطين";
     public string NativeNameTranslation => "Filasṭīn";
     public string Name => "Palestine";
 }
 
-public class Panama : ICountry
+public record Panama : ICountry
 {
     public string Name => "Panama";
 }
 
-public class Paraguay : ICountry
+public record Paraguay : ICountry
 {
     public string Name => "Paraguay";
 }
 
-public class Peru : ICountry
+public record Peru : ICountry
 {
     public string Name => "Peru";
 }
 
-public class Poland : ICountry
+public record Poland : ICountry
 {
     public string NativeName => "Polska";
     public IEnumerable<string> Languages => ["Polish"];
@@ -412,7 +412,7 @@ public class Poland : ICountry
     public string Name => "Poland";
 }
 
-public class Portugal : ICountry
+public record Portugal : ICountry
 {
     public IEnumerable<string> Languages => ["Portugese"];
     public IEnumerable<string> Regions => ["Europe"];
@@ -421,40 +421,40 @@ public class Portugal : ICountry
     public string Name => "Portugal";
 }
 
-public class Romania : ICountry
+public record Romania : ICountry
 {
     public string NativeName => "România";
     public string Name => "Romania";
 }
 
-public class Russia : ICountry
+public record Russia : ICountry
 {
     public string NativeName => "Россия";
     public string NativeNameTranslation => "Rossiya";
     public string Name => "Russia";
 }
 
-public class SaudiArabia : ICountry
+public record SaudiArabia : ICountry
 {
     public string NativeName => "ٱلسُّعُودِيَّة";
     public string NativeNameTranslation => "Suʿūdiyya";
     public string Name => "Saudi Arabia";
 }
 
-public class Slovakia : ICountry
+public record Slovakia : ICountry
 {
     public string NativeName => "Slovensko";
     public string Name => "Slovakia";
 }
 
-public class SouthKorea : ICountry
+public record SouthKorea : ICountry
 {
     public string NativeName => "한국";
     public string NativeNameTranslation => "Hanguk";
     public string Name => "South Korea";
 }
 
-public class Spain : ICountry
+public record Spain : ICountry
 {
     public string NativeName => "España";
     public IEnumerable<string> Languages => ["Spanish"];
@@ -464,46 +464,46 @@ public class Spain : ICountry
     public string Name => "Spain";
 }
 
-public class Sweden : ICountry
+public record Sweden : ICountry
 {
     public string NativeName => "Sverige";
     public string Name => "Sweden";
 }
 
-public class Switzerland : ICountry
+public record Switzerland : ICountry
 {
     public string NativeName => "Schweiz";
     public string Name => "Switzerland";
 }
 
-public class Syria : ICountry
+public record Syria : ICountry
 {
     public string NativeName => "سُورِيَا";
     public string NativeNameTranslation => "Sūriyā";
     public string Name => "Syria";
 }
 
-public class Taiwan : ICountry
+public record Taiwan : ICountry
 {
     public string NativeName => "中華";
     public string NativeNameTranslation => "Zhōnghuá Mínguó";
     public string Name => "Taiwan";
 }
 
-public class Turkey : ICountry
+public record Turkey : ICountry
 {
     public string NativeName => "Türkiye";
     public string Name => "Turkey";
 }
 
-public class Ukraine : ICountry
+public record Ukraine : ICountry
 {
     public string NativeName => "Україна";
     public string NativeNameTranslation => "Ukraina";
     public string Name => "Ukraine";
 }
 
-public class UnitedKingdom : ICountry
+public record UnitedKingdom : ICountry
 {
     public string Demonym => "British";
     public IEnumerable<string> Languages => ["English"];
@@ -513,7 +513,7 @@ public class UnitedKingdom : ICountry
     public string Name => "United Kingdom";
 }
 
-public class UnitedStates : ICountry
+public record UnitedStates : ICountry
 {
     public string Demonym => "American";
     public IEnumerable<string> Languages => ["English"];
@@ -523,17 +523,17 @@ public class UnitedStates : ICountry
     public string Name => "United States of America";
 }
 
-public class Uruguay : ICountry
+public record Uruguay : ICountry
 {
     public string Name => "Uruguay";
 }
 
-public class Venezuela : ICountry
+public record Venezuela : ICountry
 {
     public string Name => "Venezuela";
 }
 
-public class Yemen : ICountry
+public record Yemen : ICountry
 {
     public string NativeName => "ٱلْيَمَنْ";
     public string NativeNameTranslation => "al-Yaman";
