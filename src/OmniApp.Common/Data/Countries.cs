9fd922c7324d164dae20187aa541e1d29fe29e88
geckro <94733731+geckro@users.noreply.github.com>
2024-08-10 15:09:47 +0800

    feat: Add currency and currency title to ICountry and implement them

--- src/OmniApp.Common/Data/Countries.cs
+++ src/OmniApp.Common/Data/Countries.cs
@@ -9,16 +9,30 @@ public interface ICountry
     ///    The name of the country, in English.
     /// </summary>
     string Name { get; }
+
+    /// <summary>
+    ///    The currency of the country, in ISO 4217 code format.
+    /// </summary>
+    string Currency { get; }
+
+    /// <summary>
+    ///    The currency title of the country, in ISO 4217 full name format.
+    /// </summary>
+    string CurrencyTitle { get; }
 }
 
 public record Albania : ICountry
 {
+    public string Currency => "ALD";
+    public string CurrencyTitle => "Albanian Lek";
     public string NativeName => "Shqipëri";
     public string Name => "Albania";
 }
 
 public record Algeria : ICountry
 {
+    public string Currency => "DZD";
+    public string CurrencyTitle => "Algerian Dinar";
     public string NativeName => "الجزائر";
     public string NativeNameTranslation => "al-Jazāʾir";
     public string Name => "Algeria";
@@ -26,6 +40,8 @@ public record Algeria : ICountry
 
 public record Argentina : ICountry
 {
+    public string Currency => "ARS";
+    public string CurrencyTitle => "Argentine Peso";
     public string Name => "Argentina";
 }
 
@@ -37,6 +53,7 @@ public record Austria : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "AT";
     public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Name => "Austria";
 }
 
@@ -47,11 +64,14 @@ public record Australia : ICountry
     public IEnumerable<string> Regions => ["Oceania"];
     public string Iso3166Code => "AU";
     public string Currency => "AUD";
+    public string CurrencyTitle => "Australian Dollar";
     public string Name => "Australia";
 }
 
 public record Bangladesh : ICountry
 {
+    public string Currency => "BDT";
+    public string CurrencyTitle => "Bangladeshi Taka";
     public string NativeName => "বাংলাদেশ";
     public string NativeNameTranslation => "Bāṅlādēś";
     public string Name => "Bangladesh";
@@ -59,6 +79,8 @@ public record Bangladesh : ICountry
 
 public record Belarus : ICountry
 {
+    public string Currency => "BYN";
+    public string CurrencyTitle => "Belarusian Ruble";
     public string NativeName => "Беларусь";
     public string Name => "Belarus";
 }
@@ -71,22 +93,29 @@ public record Belgium : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "BE";
     public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Name => "Belgium";
 }
 
 public record Bolivia : ICountry
 {
+    public string Currency => "BOB";
+    public string CurrencyTitle => "Boliviano";
     public string Name => "Bolivia";
 }
 
 public record Brazil : ICountry
 {
+    public string Currency => "BRL";
+    public string CurrencyTitle => "Brazilian Real";
     public string NativeName => "Brasil";
     public string Name => "Brazil";
 }
 
 public record Bulgaria : ICountry
 {
+    public string Currency => "BGN";
+    public string CurrencyTitle => "Bulgarian Lev";
     public string Demonym => "Bulgarian";
     public string NativeName => "България";
     public string NativeNameTranslation => "Bŭlgariya";
@@ -99,12 +128,15 @@ public record Canada : ICountry
     public IEnumerable<string> Languages => ["English"];
     public IEnumerable<string> Regions => ["America"];
     public string Iso3166Code => "CA";
-    public string Currency => "CAD";
+    public string Currency => "CAN";
+    public string CurrencyTitle => "Canadian Dollar";
     public string Name => "Canada";
 }
 
 public record Chile : ICountry
 {
+    public string Currency => "CLP";
+    public string CurrencyTitle => "Chilean Peso";
     public string Name => "Chile";
 }
 
@@ -117,55 +149,74 @@ public record China : ICountry
     public IEnumerable<string> Regions => ["Asia"];
     public string Iso3166Code => "CN";
     public string Currency => "CNY";
+    public string CurrencyTitle => "Chinese Yuan";
     public string Name => "China (People's Republic)";
 }
 
 public record Colombia : ICountry
 {
+    public string Currency => "COP";
+    public string CurrencyTitle => "Colombian Peso";
     public string Name => "Colombia";
 }
 
 public record CostaRica : ICountry
 {
+    public string Currency => "CRC";
+    public string CurrencyTitle => "Costa Rican Colon";
     public string Name => "Costa Rica";
 }
 
 public record Croatia : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Hrvatska";
     public string Name => "Croatia";
 }
 
 public record Cuba : ICountry
 {
+    public string Currency => "CUP";
+    public string CurrencyTitle => "Cuban Peso";
     public string Name => "Cuba";
 }
 
 public record Czechia : ICountry
 {
+    public string Currency => "CZK";
+    public string CurrencyTitle => "Czech Koruna";
     public string NativeName => "Česko";
     public string Name => "Czech Republic";
 }
 
 public record Denmark : ICountry
 {
+    public string Currency => "DKK";
+    public string CurrencyTitle => "Danish Krone";
     public string NativeName => "Danmark";
     public string Name => "Denmark";
 }
 
 public record DominicanRepublic : ICountry
 {
+    public string Currency => "DOP";
+    public string CurrencyTitle => "Dominican Peso";
     public string NativeName => "República Dominicana";
     public string Name => "Dominican Republic";
 }
 
 public record Ecuador : ICountry
 {
+    public string Currency => "USD";
+    public string CurrencyTitle => "United States Dollar";
     public string Name => "Ecuador";
 }
 
 public record Egypt : ICountry
 {
+    public string Currency => "EGP";
+    public string CurrencyTitle => "Egyptian Pound";
     public string NativeName => "مصر";
     public string NativeNameTranslation => "Miṣr";
     public string Name => "Egypt";
@@ -173,29 +224,39 @@ public record Egypt : ICountry
 
 public record ElSalvador : ICountry
 {
+    public string Currency => "SVC";
+    public string CurrencyTitle => "Salvadoran Colon";
     public string Name => "El Salvador";
 }
 
 public record Estonia : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Eesti";
     public string Name => "Estonia";
 }
 
 public record EuropeanUnion : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Iso3166Code => "EU";
     public string Name => "European Union";
 }
 
 public record Finland : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Suomi";
     public string Name => "Finland";
 }
 
 public record France : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Name => "France";
 }
 
@@ -207,11 +268,14 @@ public record Germany : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "DE";
     public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Name => "Germany";
 }
 
 public record Greece : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Ελλάδα";
     public string NativeNameTranslation => "Elláda";
     public string Name => "Greece";
@@ -219,28 +283,38 @@ public record Greece : ICountry
 
 public record Guatemala : ICountry
 {
+    public string Currency => "GTQ";
+    public string CurrencyTitle => "Guatemalan Quetzal";
     public string Name => "Guatemala";
 }
 
 public record Haiti : ICountry
 {
+    public string Currency => "HTG";
+    public string CurrencyTitle => "Haitian Gourde";
     public string NativeName => "Ayiti";
     public string Name => "Haiti";
 }
 
 public record Honduras : ICountry
 {
+    public string Currency => "HNL";
+    public string CurrencyTitle => "Honduran Lempira";
     public string Name => "Honduras";
 }
 
 public record Hungary : ICountry
 {
+    public string Currency => "HUF";
+    public string CurrencyTitle => "Hungarian Forint";
     public string NativeName => "Magyarország";
     public string Name => "Hungary";
 }
 
 public record India : ICountry
 {
+    public string Currency => "INR";
+    public string CurrencyTitle => "Indian Rupee";
     public string NativeName => "भारत";
     public string NativeNameTranslation => "Bhaarat";
     public string Name => "India";
@@ -248,17 +322,23 @@ public record India : ICountry
 
 public record Indonesia : ICountry
 {
+    public string Currency => "IDR";
+    public string CurrencyTitle => "Indonesian Rupiah";
     public string Name => "Indonesia";
 }
 
 public record Iran : ICountry
 {
+    public string Currency => "IRR";
+    public string CurrencyTitle => "Iranian Rial";
     public string NativeName => "ایران";
     public string Name => "Iran";
 }
 
 public record Iraq : ICountry
 {
+    public string Currency => "IQD";
+    public string CurrencyTitle => "Iraqi Dollar";
     public string NativeName => "ٱلْعِرَاق";
     public string NativeNameTranslation => "Êraq";
     public string Name => "Iraq";
@@ -266,12 +346,16 @@ public record Iraq : ICountry
 
 public record Ireland : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Éire";
     public string Name => "Ireland";
 }
 
 public record Israel : ICountry
 {
+    public string Currency => "ILS";
+    public string CurrencyTitle => "Israeli New Shekel";
     public string NativeName => "יִשְׂרָאֵל";
     public string NativeNameTranslation => "Yīsrāʾēl";
     public string Name => "Israel";
@@ -279,11 +363,12 @@ public record Israel : ICountry
 
 public record Italy : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Italia";
     public IEnumerable<string> Languages => ["Italian"];
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "IT";
-    public string Currency => "EUR";
     public string Name => "Italy";
 }
 
@@ -296,17 +381,22 @@ public record Japan : ICountry
     public IEnumerable<string> Regions => ["Asia"];
     public string Iso3166Code => "JP";
     public string Currency => "JPY";
+    public string CurrencyTitle => "Japanese Yen";
     public string Name => "Japan";
 }
 
 public record Latvia : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Latvijas";
     public string Name => "Latvia";
 }
 
 public record Libya : ICountry
 {
+    public string Currency => "LYD";
+    public string CurrencyTitle => "Libyan Dinar";
     public string NativeName => "ليبيا";
     public string NativeNameTranslation => "Lībiyā";
     public string Name => "Libya";
@@ -314,28 +404,38 @@ public record Libya : ICountry
 
 public record Lithuania : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Lietuva";
     public string Name => "Lithuania";
 }
 
 public record Luxembourg : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Lëtzebuerg";
     public string Name => "Luxembourg";
 }
 
 public record Malaysia : ICountry
 {
+    public string Currency => "MYR";
+    public string CurrencyTitle => "Malaysian Ringgit";
     public string Name => "Malaysia";
 }
 
 public record Mexico : ICountry
 {
+    public string Currency => "MXN";
+    public string CurrencyTitle => "Mexican Peso";
     public string Name => "Mexico";
 }
 
 public record Moldova : ICountry
 {
+    public string Currency => "MDL";
+    public string CurrencyTitle => "Moldovan Leu";
     public string Name => "Moldova";
 }
 
@@ -346,34 +446,45 @@ public record Netherlands : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "NL";
     public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Name => "Netherlands";
 }
 
 public record NewZealand : ICountry
 {
+    public string Currency => "NZD";
+    public string CurrencyTitle => "New Zealand Dollar";
     public string NativeName => "Aotearoa";
     public string Name => "New Zealand";
 }
 
 public record Nicaragua : ICountry
 {
+    public string Currency => "NIO";
+    public string CurrencyTitle => "Nicaraguan Cordoba";
     public string Name => "Nicaragua";
 }
 
 public record Nigeria : ICountry
 {
+    public string Currency => "NGN";
+    public string CurrencyTitle => "Nigerian Naira";
     public string NativeName => "Najeriya";
     public string Name => "Nigeria";
 }
 
 public record NorthAmerica : ICountry
 {
+    public string Currency => "N/A";
+    public string CurrencyTitle => "N/A";
     public string Iso3166Code => "NA";
     public string Name => "North America";
 }
 
 public record NorthKorea : ICountry
 {
+    public string Currency => "KPW";
+    public string CurrencyTitle => "North Korean Won";
     public string NativeName => "조선";
     public string NativeNameTranslation => "Chosŏn";
     public string Name => "North Korea";
@@ -381,12 +492,16 @@ public record NorthKorea : ICountry
 
 public record Norway : ICountry
 {
+    public string Currency => "NOK";
+    public string CurrencyTitle => "Norwegian Krone";
     public string NativeName => "Norge";
     public string Name => "Norway";
 }
 
 public record Oman : ICountry
 {
+    public string Currency => "OMR";
+    public string CurrencyTitle => "Omani Rial";
     public string NativeName => "عُمَان";
     public string NativeNameTranslation => "ʿUmān";
     public string Name => "Oman";
@@ -394,12 +509,16 @@ public record Oman : ICountry
 
 public record Pakistan : ICountry
 {
+    public string Currency => "PKR";
+    public string CurrencyTitle => "Pakistani Rupee";
     public string NativeName => "پَاکِسْتَان";
     public string Name => "Pakistan";
 }
 
 public record Palestine : ICountry
 {
+    public string Currency => "N/A";
+    public string CurrencyTitle => "N/A";
     public string NativeName => "فلسطين";
     public string NativeNameTranslation => "Filasṭīn";
     public string Name => "Palestine";
@@ -407,16 +526,22 @@ public record Palestine : ICountry
 
 public record Panama : ICountry
 {
+    public string Currency => "PAB";
+    public string CurrencyTitle => "Panamanian Balboa";
     public string Name => "Panama";
 }
 
 public record Paraguay : ICountry
 {
+    public string Currency => "PYG";
+    public string CurrencyTitle => "Paraguayan Guarani";
     public string Name => "Paraguay";
 }
 
 public record Peru : ICountry
 {
+    public string Currency => "PEN";
+    public string CurrencyTitle => "Peruvian Sol";
     public string Name => "Peru";
 }
 
@@ -427,6 +552,7 @@ public record Poland : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "PL";
     public string Currency => "PLN";
+    public string CurrencyTitle => "Polish Zloty";
     public string Name => "Poland";
 }
 
@@ -436,17 +562,22 @@ public record Portugal : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "PT";
     public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string Name => "Portugal";
 }
 
 public record Romania : ICountry
 {
+    public string Currency => "RON";
+    public string CurrencyTitle => "Romanian Leu";
     public string NativeName => "România";
     public string Name => "Romania";
 }
 
 public record Russia : ICountry
 {
+    public string Currency => "RUB";
+    public string CurrencyTitle => "Russian Ruble";
     public string NativeName => "Россия";
     public string NativeNameTranslation => "Rossiya";
     public string Name => "Russia";
@@ -454,6 +585,8 @@ public record Russia : ICountry
 
 public record SaudiArabia : ICountry
 {
+    public string Currency => "SAR";
+    public string CurrencyTitle => "Saudi Riyal";
     public string NativeName => "ٱلسُّعُودِيَّة";
     public string NativeNameTranslation => "Suʿūdiyya";
     public string Name => "Saudi Arabia";
@@ -461,12 +594,16 @@ public record SaudiArabia : ICountry
 
 public record Slovakia : ICountry
 {
+    public string Currency => "EUR";
+    public string CurrencyTitle => "Euro";
     public string NativeName => "Slovensko";
     public string Name => "Slovakia";
 }
 
 public record SouthKorea : ICountry
 {
+    public string Currency => "KRW";
+    public string CurrencyTitle => "South Korean Won";
     public string NativeName => "한국";
     public string NativeNameTranslation => "Hanguk";
     public string Name => "South Korea";
@@ -479,23 +616,29 @@ public record Spain : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "ES";
     public string Currency => "EUR";
-    public string Name => "Spain";
+    public string CurrencyTitle => "Euro";    public string Name => "Spain";
 }
 
 public record Sweden : ICountry
 {
+    public string Currency => "SEK";
+    public string CurrencyTitle => "Swedish Krona";
     public string NativeName => "Sverige";
     public string Name => "Sweden";
 }
 
 public record Switzerland : ICountry
 {
+    public string Currency => "CHE";
+    public string CurrencyTitle => "WIR Euro";
     public string NativeName => "Schweiz";
     public string Name => "Switzerland";
 }
 
 public record Syria : ICountry
 {
+    public string Currency => "SYP";
+    public string CurrencyTitle => "Syrian Pound";
     public string NativeName => "سُورِيَا";
     public string NativeNameTranslation => "Sūriyā";
     public string Name => "Syria";
@@ -503,6 +646,8 @@ public record Syria : ICountry
 
 public record Taiwan : ICountry
 {
+    public string Currency => "TWD";
+    public string CurrencyTitle => "New Taiwan Dollar";
     public string NativeName => "中華";
     public string NativeNameTranslation => "Zhōnghuá Mínguó";
     public string Name => "Taiwan";
@@ -510,12 +655,16 @@ public record Taiwan : ICountry
 
 public record Turkey : ICountry
 {
+    public string Currency => "TRY";
+    public string CurrencyTitle => "Turkish Lira";
     public string NativeName => "Türkiye";
     public string Name => "Turkey";
 }
 
 public record Ukraine : ICountry
 {
+    public string Currency => "UAH";
+    public string CurrencyTitle => "Ukrainian Hryvnia";
     public string NativeName => "Україна";
     public string NativeNameTranslation => "Ukraina";
     public string Name => "Ukraine";
@@ -528,6 +677,7 @@ public record UnitedKingdom : ICountry
     public IEnumerable<string> Regions => ["Europe"];
     public string Iso3166Code => "GB";
     public string Currency => "GBP";
+    public string CurrencyTitle => "Pound Sterling";
     public string Name => "United Kingdom";
 }
 
@@ -538,21 +688,28 @@ public record UnitedStates : ICountry
     public IEnumerable<string> Regions => ["America", "South America"];
     public string Iso3166Code => "US";
     public string Currency => "USD";
+    public string CurrencyTitle => "United States Dollar";
     public string Name => "United States of America";
 }
 
 public record Uruguay : ICountry
 {
+    public string Currency => "UYU";
+    public string CurrencyTitle => "Uruguayan Peso";
     public string Name => "Uruguay";
 }
 
 public record Venezuela : ICountry
 {
+    public string Currency => "VES";
+    public string CurrencyTitle => "Venezuelan Sovereign Bolivar";
     public string Name => "Venezuela";
 }
 
 public record Yemen : ICountry
 {
+    public string Currency => "YER";
+    public string CurrencyTitle => "Yemeni Rial";
     public string NativeName => "ٱلْيَمَنْ";
     public string NativeNameTranslation => "al-Yaman";
     public string Name => "Yemen";
